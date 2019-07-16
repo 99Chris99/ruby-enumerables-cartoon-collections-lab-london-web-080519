11e784539fc6dd8299cb8d7b1f6b466c6bbc656c
Chris Hodge <chrishodge87@gmail.com>
2019-07-16 10:45:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,13 +3,13 @@ def roll_call_dwarves(dwarfs)# code an argument here
 #  dwarfs.sort!
 dwarfs.each_with_index
 
-do |item, index|
+{ |item, index|
 
 index +1
 puts "#{index} #{item}"
 #return dwarfs
 
-end
+}
 
 end
 
