cd8fb24592b071429900aa7ccaebb319623c0cff
Chris Hodge <chrishodge87@gmail.com>
2019-07-16 10:11:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs)# code an argument here
+dwarfs.each_with_index{|item, index|}
+p dwarfs
+  
 end
 
 def summon_captain_planet# code an argument here
